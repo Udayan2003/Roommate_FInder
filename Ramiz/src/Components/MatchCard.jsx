13361c3e54f0712fa1e50a3d7235be0fd432f0e0
Udayan <udayan.21bce7045@vitapstudent.ac.in>
2023-05-05 03:02:54 +0530

    Add Everything xD
    - Modify Register page to add Reg.No
    - Route and chain all pages together

--- Ramiz/src/Components/MatchCard.jsx
+++ Ramiz/src/Components/MatchCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function MatchCard({ name }) {
-    if (!name) return (<></>);
+function MatchCard({name}) {
+    if (!name) return (<>{console.log("Ain't working")}</>);
     return (
         <div className="flex flex-wrap justify-center">
             <div className="w-1/2 bg-gray-800 m-4 rounded-lg shadow-lg">
                 <div className="p-4 flex flex-wrap flex-row flex-start">
-                    <h1 className="text-2xl text-white font-bold">{ name }</h1>
+                    <h1 className="text-2xl text-white font-bold"> { name } </h1>
                     <button className="ml-auto bg-red-700 text-white px-4 py-2 rounded-lg">More</button>
                 </div>
             </div>
