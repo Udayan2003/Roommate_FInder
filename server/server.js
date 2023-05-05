2538aacbac84f8a71afe6638dc7c522058c8c240
FowlDeadPool <95326726+FowlDeadPool@users.noreply.github.com>
2023-05-05 17:57:50 +0530

    changes in button

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.get("/api", (req, res) => {
     res.json({"users":["userOne", "userTwo", "userThree"]})
 })
 
-app.listen(5000, () => { console.log("Server Started on port 5000")})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Store the data in a separate JSON file
+    const user = {
+      email: email,
+      password: password
+    }
+    fs.writeFile('./data.json', JSON.stringify(user), (err) => {
+      if (err) throw err;
+      console.log('User data has been saved!');
+    });
+
+    // Redirect to the Details page
+    res.redirect('/details');
+});
+
+app.listen(5000, () => { console.log("Server Started on port 5000")});
+
